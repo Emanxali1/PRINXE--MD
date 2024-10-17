25d2f5b6357868dabcc1fac8d0a9ee8f021ff791
Emanxali1 <yyyking77998@outlook.com>
2024-10-17 21:36:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,8 +27,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
-global.prems = ['']
+global.mods = ['923318086563'] 
+global.prems = ['923318086563']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
